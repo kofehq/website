7d1d3bf2b62859cf1d7cf668aaafe29404268561
Leonardo Koslowski <leok85@gmail.com>
2021-08-13 17:22:59 -0300

    :sparkles: Disable AOS for internet bots

--- plugins/aos.js
+++ plugins/aos.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
-Vue.use(AOS.init({
-  disable: 'phone'
-}))
+const botRegex = /bot|googlebot|crawler|spider|robot|crawling/i
+const isBot = navigator.userAgent && botRegex.test(navigator.userAgent)
+
+if (!isBot) {
+  Vue.use(AOS.init({
+    disable: 'phone'
+  }))
+}
+
