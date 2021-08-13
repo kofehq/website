fa3b6727a63cd32f45a4467af0f12378fb243005
Leonardo Koslowski <leok85@gmail.com>
2021-08-13 17:42:05 -0300

    :wheelchair: Add chrome lighthouse in bot regex

--- plugins/aos.js
+++ plugins/aos.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
-const botRegex = /bot|googlebot|crawler|spider|robot|crawling/i
+const botRegex = /bot|googlebot|crawler|spider|robot|crawling|Chrome-Lighthouse/i
 const isBot = navigator.userAgent && botRegex.test(navigator.userAgent)
 
 if (!isBot) {
