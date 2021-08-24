9a2d3c7bb14a8d31f97cb471ca95d3b5b4537c16
Leonardo Koslowski <leok85@gmail.com>
2021-08-23 21:52:51 -0300

    :bug: Fix mobile link crash

--- plugins/aos.js
+++ plugins/aos.js
@@ -5,11 +5,16 @@ import 'aos/dist/aos.css'
 const botRegex = /bot|googlebot|crawler|spider|robot|crawling|Chrome-Lighthouse/i
 const isBot = navigator.userAgent && botRegex.test(navigator.userAgent)
 
-if (!isBot && window.innerWidth >= 768) {
-  Vue.use(AOS.init({
-    disable: () => {
-      return window.innerWidth < 768
-    }
-  }))
+const aosPlugin = {
+  install: () => {
+    AOS.init({
+      disable: () => {
+        return window.innerWidth < 768 || isBot
+      }
+    })
+  },
 }
 
+if (process.client) {
+  Vue.use(aosPlugin)
+}
